d503407c7caa4aacba9e2f75839b489df6a8030d
NamPNQ <nampnq@gmail.com>
2014-04-12 00:48:00 +0700

    Update youtube.js
    Fix something

--- youtube.js
+++ youtube.js
@@ -1,10 +1,10 @@
 "use strict";
 angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
     .run(['$rootScope', '$window',
-        function($rootScope, $window) {
+        function ($rootScope, $window) {
             $rootScope.youtubeApiReady = false;
-            $window.onYouTubeIframeAPIReady = function() {
-                $rootScope.$apply(function() {
+            $window.onYouTubeIframeAPIReady = function () {
+                $rootScope.$apply(function () {
                     $rootScope.youtubeApiReady = true;
                 });
 
@@ -17,20 +17,21 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
         }
     ])
     .directive(
-        "vgYoutube", ["VG_EVENTS", "VG_STATES", "$rootScope", "$window",
-            function(VG_EVENTS, VG_STATES, $rootScope, $window) {
+        "vgYoutube", ["VG_EVENTS", "VG_STATES", "$rootScope", "$window", "$timeout", "$interval",
+            function (VG_EVENTS, VG_STATES, $rootScope, $window, $timeout, $interval) {
                 return {
                     restrict: "E",
                     require: "^videogular",
-                    templateUrl: "views/videogular/plugins/youtube/youtube.html",
+                    templateUrl: "/application/js/libs/videogular/plugins/views/youtube/youtube.html",
                     scope: {},
-                    link: function(scope, elem, attr, API) {
+                    link: function (scope, elem, attr, API) {
                         var result = {
                             method: "",
                             url: ""
                         };
+                        var isFirefox = typeof InstallTrigger !== 'undefined';
                         scope.vgYoutubePlayerId = Date.now();
-                        scope.onYoutubeStateChange = function(event) {
+                        var onYoutubeStateChange = function (event) {
                             var videogularElementScope = API.elementScope.scope().$$childHead;
                             if (event.data == YT.PlayerState.BUFFERING) {
                                 videogularElementScope.onStartBuffering({
@@ -48,66 +49,84 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
                                 API.pause();
                             }
                         }
-                        scope.onVideoReady = function() {
+                        var onVideoReady = function () {
                             var videogularElementScope = API.elementScope.scope().$$childHead,
-                                vgOverPlayElementScope = angular.element(document.querySelector('vg-overlay-play>div'), API.videogularElement).scope();
+                                vgOverPlayElementScope = angular.element('vg-overlay-play>div', API.videogularElement).scope();
                             vgOverPlayElementScope.currentIcon = vgOverPlayElementScope.playIcon;
-                            API.videoElement.remove();
-                            API.videoElement = angular.element(document.querySelector("#youtube_player_" + scope.vgYoutubePlayerId));
+                            API.videoElement.remove();// = null;
+                            API.videoElement = angular.element("#youtube_player_" + scope.vgYoutubePlayerId);
+                            if (isFirefox){
+                                API.videoElement.css('visibility','hidden')
+                            }
+                            else{
+                                API.videoElement.css('display', 'none');
+                            }
+                            //Define some property, method for player
+                            API.videoElement[0].__defineGetter__("currentTime", function () {
+                                return scope.ytplayer.getCurrentTime();
+                            });
+                            API.videoElement[0].__defineSetter__("currentTime", function (seconds) {
+                                return scope.ytplayer.seekTo(seconds, true);
+                            });
+                            API.videoElement[0].__defineGetter__("duration", function () {
+                                return scope.ytplayer.getDuration();
+                            });
+                            API.videoElement[0].__defineGetter__("paused", function () {
+                                return scope.ytplayer.getPlayerState() != YT.PlayerState.PLAYING;
+                            });
+                            API.videoElement[0].__defineGetter__("videoWidth", function () {
+                                return scope.ytplayer.a.width;
+                            });
+                            API.videoElement[0].__defineGetter__("videoHeight", function () {
+                                return scope.ytplayer.a.height;
+                            });
+                            API.videoElement[0].__defineGetter__("volume", function () {
+                                return scope.ytplayer.getVolume() / 100.0;
+                            });
+                            API.videoElement[0].__defineSetter__("volume", function (volume) {
+                                return scope.ytplayer.setVolume(volume * 100.0);
+                            });
+                            API.videoElement[0].play = function () {
+                                if (isFirefox){
+                                    API.videoElement.css('visibility','visible')
+                                }
+                                else{
+                                    API.videoElement.css('display', 'block');
+                                }
+                                scope.ytplayer.playVideo();
+                            }
+                            API.videoElement[0].pause = function () {
+                                if (isFirefox){
+                                    API.videoElement.css('visibility','hidden')
+                                }
+                                else{
+                                    API.videoElement.css('display', 'none');
+                                }
+                                scope.ytplayer.pauseVideo();
+                            };
+                            scope.ytplayer.setSize(API.getSize().width, API.getSize().height);
+                            videogularElementScope.updateSize();
+                            function updateTime(){
+                                videogularElementScope.onUpdateTime({
+                                        target: API.videoElement[0]
+                                    })
+                            }
+                            scope.updateTimer = setInterval(updateTime, 600);
                             //Overwrite method onPlayerReady in videogularElementScope
-                            videogularElementScope.onPlayerReady = function() {
+
+                            videogularElementScope.onPlayerReady = function () {
                                 videogularElementScope.doPlayerReady();
                             };
 
                             //Call method onVideoReady in videogularElementScope
                             videogularElementScope.onVideoReady();
-                            //Define some property, method for player
-                            API.videoElement[0].__defineGetter__("currentTime", function() {
-                                return scope.ytplayer.getCurrentTime();
-                            });
-                            API.videoElement[0].__defineSetter__("currentTime", function(seconds) {
-                                return scope.ytplayer.seekTo(seconds, true);
-                            });
-                            API.videoElement[0].__defineGetter__("duration", function() {
-                                return scope.ytplayer.getDuration();
-                            });
-                            API.videoElement[0].__defineGetter__("paused", function() {
-                                return scope.ytplayer.getPlayerState() != YT.PlayerState.PLAYING;
-                            });
-                            API.videoElement[0].__defineGetter__("videoWidth", function() {
-                                return scope.ytplayer.a.width;
-                            });
-                            API.videoElement[0].__defineGetter__("videoHeight", function() {
-                                return scope.ytplayer.a.height;
-                            });
-                            API.videoElement[0].__defineGetter__("volume", function() {
-                                return scope.ytplayer.getVolume() / 100.0;
-                            });
-                            API.videoElement[0].__defineSetter__("volume", function(volume) {
-                                return scope.ytplayer.setVolume(volume * 100.0);
-                            });
-                            API.videoElement[0].play = function() {
-                                API.videoElement.css('display', 'block');
-                                scope.ytplayer.playVideo();
-                            }
-                            API.videoElement[0].pause = function() {
-                                API.videoElement.css('display', 'none');
-                                scope.ytplayer.pauseVideo();
-                            };
-                            scope.ytplayer.setSize(API.getSize().width, API.getSize().height);
-                            videogularElementScope.updateSize();
-                            setInterval(function() {
-                                videogularElementScope.onUpdateTime({
-                                    target: API.videoElement[0]
-                                })
-                            }, 600);
-
-
                         }
-                        scope.loadYoutube = function() {
+                        scope.loadYoutube = function () {
                             var videogularElementScope = API.elementScope.scope().$$childHead,
-                                vgOverPlayElementScope = angular.element(document.querySelector('vg-overlay-play>div'), API.videogularElement).scope();
-                            vgOverPlayElementScope.currentIcon = "";
+                                vgOverPlayElementScope = angular.element('vg-overlay-play>div', API.videogularElement).scope();
+                            if(vgOverPlayElementScope){
+                                vgOverPlayElementScope.currentIcon = "";
+                            }
                             scope.ytplayer = new YT.Player('youtube_player_' + scope.vgYoutubePlayerId, {
                                 height: API.getSize().height,
                                 width: API.getSize().width,
@@ -117,12 +136,12 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
                                     showinfo: 0
                                 },
                                 events: {
-                                    'onReady': scope.onVideoReady,
-                                    'onStateChange': scope.onYoutubeStateChange
+                                    'onReady': onVideoReady,
+                                    'onStateChange': onYoutubeStateChange
                                 }
                             });
                         }
-                        scope.parseSrc = function(src) {
+                        scope.parseSrc = function (src) {
                             if (src) {
                                 // Regex to parse the video ID
                                 var regId = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
@@ -156,20 +175,22 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
                                 }
                             }
                         };
-                        $rootScope.$watch('youtubeApiReady', function(value) {
+                        $rootScope.$watch('youtubeApiReady', function (value) {
                             if (value) {
                                 console.log("Api loaded..");
                                 if (result.method === 'youtube')
-                                    scope.loadYoutube();
+                                    $timeout(function () {
+                                        scope.loadYoutube()
+                                    });
                             }
                         })
-                        scope.removeHtmlMediaElementListener = function(htmlMediaElement) {
+                        scope.removeHtmlMediaElementListener = function (htmlMediaElement) {
                             htmlMediaElement.removeEventListener("waiting");
                             htmlMediaElement.removeEventListener("ended");
                             htmlMediaElement.removeEventListener("playing");
                             htmlMediaElement.removeEventListener("timeupdate");
                         }
-                        scope.checkYoutubeSource = function() {
+                        scope.checkYoutubeSource = function () {
                             var htmlMediaElement = API.videoElement[0];
                             var mediaFiles = [],
                                 i,
@@ -218,11 +239,14 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
                                     console.log("Please check youtube video in source");
                                 }
                             } else if (result.method === 'youtube') {
-                                scope.removeHtmlMediaElementListener(htmlMediaElement);
+                                //htmlMediaElement = null;
                                 scope.parseSrc(result.url);
                             }
                         };
                         scope.checkYoutubeSource();
+                        scope.$on('$destroy', function () {
+                            clearInterval(scope.updateTimer);
+                        })
 
                     }
                 }
