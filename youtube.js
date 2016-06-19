c4aaa59af0934aded551f636c5739068a64f6a7d
Ashlin Allen <ashlin.allen@gmail.com>
2016-06-19 14:30:30 -0700

    Added iv_load_policy parameter so annotation display can be configured

--- youtube.js
+++ youtube.js
@@ -30,7 +30,8 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
                             'showinfo': 0,
                             'rel': 0,
                             'autoplay': 0, //Switch autoplay to 1 to autoplay videos
-                            'start': 0
+                            'start': 0,
+                            'iv_load_policy': 1
                         };
 
                         if (optionsArr !== null) {
