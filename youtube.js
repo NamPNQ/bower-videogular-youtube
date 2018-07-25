3ab5c0729b62f3295739d4ef738a86d1e5df433d
Ariel Barreiro <abarrei@gmail.com>
2018-07-25 16:15:01 -0300

    Allow for any youtube player vars

--- youtube.js
+++ youtube.js
@@ -25,21 +25,12 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
 
                         var youtubeReg = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
                         optionsArr = attr.vgYoutube !== null ? attr.vgYoutube.split(";") : null;
-                        playerVars = {
-                            'controls': 0,
-                            'showinfo': 0,
-                            'rel': 0,
-                            'autoplay': 0, //Switch autoplay to 1 to autoplay videos
-                            'start': 0,
-                            'iv_load_policy': 1
-                        };
+                        playerVars = {};
 
                         if (optionsArr !== null) {
                             optionsArr.forEach(function (item) {
                                 var keyValuePair = item.split("=");
-                                if (playerVars.hasOwnProperty(keyValuePair[0])) {
-                                  playerVars[keyValuePair[0]] = keyValuePair[1] || 0;
-                                }
+                                playerVars[keyValuePair[0]] = keyValuePair[1] || 0;
                             });
                         }
 
